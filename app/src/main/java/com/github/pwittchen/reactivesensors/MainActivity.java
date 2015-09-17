0b602eaa5e8784a680c4670f3f50d393f777e87b
Piotr Wittchen <piotr@wittchen.biz.pl>
2015-09-17 22:30:44 +0200

    ༼ つ ◕_◕ ༽つ giving more samples with various sensors

--- app/src/main/java/com/github/pwittchen/reactivesensors/MainActivity.java
+++ app/src/main/java/com/github/pwittchen/reactivesensors/MainActivity.java
@@ -19,8 +19,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import com.github.pwittchen.reactivesensors.samples.AccelerometerActivity;
+import com.github.pwittchen.reactivesensors.samples.AmbientTemperatureActivity;
+import com.github.pwittchen.reactivesensors.samples.GravityActivity;
 import com.github.pwittchen.reactivesensors.samples.GyroscopeActivity;
+import com.github.pwittchen.reactivesensors.samples.LightActivity;
+import com.github.pwittchen.reactivesensors.samples.LinearAccelerationActivity;
+import com.github.pwittchen.reactivesensors.samples.MagneticFieldActivity;
 import com.github.pwittchen.reactivesensors.samples.OrientationActivity;
+import com.github.pwittchen.reactivesensors.samples.PressureActivity;
+import com.github.pwittchen.reactivesensors.samples.ProximityActivity;
+import com.github.pwittchen.reactivesensors.samples.RelativeHumidityActivity;
+import com.github.pwittchen.reactivesensors.samples.RotationVectorActivity;
+import com.github.pwittchen.reactivesensors.samples.TemperatureActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +40,17 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     findViewAndSetOnClickListener(R.id.b_gyroscope, GyroscopeActivity.class);
     findViewAndSetOnClickListener(R.id.b_orientation, OrientationActivity.class);
+    findViewAndSetOnClickListener(R.id.b_accelerometer, AccelerometerActivity.class);
+    findViewAndSetOnClickListener(R.id.b_ambient_temperature, AmbientTemperatureActivity.class);
+    findViewAndSetOnClickListener(R.id.b_gravity, GravityActivity.class);
+    findViewAndSetOnClickListener(R.id.b_light, LightActivity.class);
+    findViewAndSetOnClickListener(R.id.b_linear_acceleration, LinearAccelerationActivity.class);
+    findViewAndSetOnClickListener(R.id.b_magnetic_field, MagneticFieldActivity.class);
+    findViewAndSetOnClickListener(R.id.b_pressure, PressureActivity.class);
+    findViewAndSetOnClickListener(R.id.b_proximity, ProximityActivity.class);
+    findViewAndSetOnClickListener(R.id.b_relative_humidity, RelativeHumidityActivity.class);
+    findViewAndSetOnClickListener(R.id.b_rotation_vector, RotationVectorActivity.class);
+    findViewAndSetOnClickListener(R.id.b_temperature, TemperatureActivity.class);
   }
 
   private void findViewAndSetOnClickListener(int viewId, final Class<?> cls) {
